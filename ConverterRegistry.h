df055f3d8fc89e2f3a6b23e68ab8a83f274e6e49
Ivan Tro <psychofuel@gmail.com>
2015-01-08 13:23:20 +0300

    converter registry

--- ConverterRegistry.h
+++ ConverterRegistry.h
@@ -7,15 +7,35 @@
 
 #import <Foundation/Foundation.h>
 
-
+/**
+ * A class for organizing converters.
+ *
+ * <p>Contains method for registering and obtaining type converters</p>
+ *
+ */
 @interface ConverterRegistry : NSObject
 
-+ (id)registerConverterBySource:(id)sourse byTarget:(id)target andConvertor:(id)Convertor;
 
-//+ (id)registerConverterUntyped
++ (ConverterRegistry *)sharedConverter;
 
-//- (id)getConverterBySource:(id)sourse andTarget:(id)target;
+/**
+ * Register specified converter in this converter registry.
+ * @param source Class source type
+ * @param target Class target type
+ * @param converter converter instance
+ * @return this object for easy chaining
+ */
+- (ConverterRegistry *)registerConverterBySource:(id)sourse byTarget:(id)target andConvertor:(id)convertor;
 
-//- (int)converterHashCode;
+/**
+ * Returns type converter for specified obj-c types.
+ *
+ * @param source Class source java type
+ * @param target Class target java type
+ * @return an instance of {@link Converter}.
+ *         <code>null</code> assert - no converter is found.
+ */
+- (id)getConverterBySource:(id)sourse andTarget:(id)target;
+
 
 @end
--- ConverterRegistry.m
+++ ConverterRegistry.m
@@ -8,11 +8,65 @@
 #import "ConverterRegistry.h"
 
 
+@interface ConverterRegistry ()
+
+@property (nonatomic, strong) NSMutableDictionary *converters;
+
+@end
+
+
 @implementation ConverterRegistry
 
-+ (id)registerConverterBySource:(id)sourse byTarget:(id)target andConvertor:(id)Convertor {
-    //TODO: need generate hash code 
-    return nil;
+#pragma mark - Allocation
+
++ (ConverterRegistry *)sharedConverter {
+    static ConverterRegistry *Converter = nil;
+    
+    @synchronized(self) {
+        if (Converter == nil) {
+            Converter = [ConverterRegistry new];
+        }
+    }
+    
+    return Converter;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _converters = [NSMutableDictionary new];
+    }
+    return self;
+}
+
+#pragma  mark - Public
+
+- (ConverterRegistry *)registerConverterBySource:(id)sourse byTarget:(id)target andConvertor:(id)convertor {
+    return [self registerConverterInternalBySource:sourse byTarget:target andConvertor:convertor];
+}
+
+- (id)getConverterBySource:(id)sourse andTarget:(id)target {
+    NSString *mappingKey = [self converterUnicKeyBySource:[sourse class] andTargetClass:[target class]];
+    id result = [self.converters objectForKey:mappingKey];
+    NSAssert((result == nil), @"Converter from '%@' to '%@' unsupported", NSStringFromClass([sourse class]), NSStringFromClass([target class]));
+    return result;
+}
+
+
+#pragma  mark - Private
+
+- (ConverterRegistry *)registerConverterInternalBySource:(id)sourse byTarget:(id)target andConvertor:(id)convertor {
+    NSString *mappingKey = [self converterUnicKeyBySource:[sourse class] andTargetClass:[target class]];
+    NSAssert(([self.converters objectForKey:mappingKey] != nil), @"Converter from '%@' to '%@' already registered.", NSStringFromClass([sourse class]), NSStringFromClass([target class]));
+   
+    @synchronized (self) {
+        [self.converters setObject:convertor forKey:mappingKey];
+    }
+    
+    return self;
+}
+
+- (NSString *)converterUnicKeyBySource:(Class)source andTargetClass:(Class)target {
+    return [NSString stringWithFormat:@"from%@to%@", NSStringFromClass(source), NSStringFromClass(target)];
 }
 
 @end
